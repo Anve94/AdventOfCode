c894551eba66bfc912e60010ca4283853b278ad9
Stefan Boonstra <anve_nomuree@hotmail.com>
2017-12-05 12:19:54 +0100

    Day 3
    Finished 3-1 and started 3-2

---
+++ 3-2.py
@@ -0,0 +1,13 @@
+"""
+Day 2 uses the same spiral. But now, the next value in the spiral
+is the sum of all values surrounding the previous value, including
+diagonals. I.e.
+
+147  142  133  122   59
+304    5    4    2   57
+330   10    1    1   54
+351   11   23   25   26
+362  747  806--->   ...
+
+Objective: find first value higher then 325489
+"""
